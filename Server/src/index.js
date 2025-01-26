99489d71edaf89ccb2eafd861dea7a980b9d2891
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-01-26 13:32:09 +0530

    add cookie-parser and cors middleware, update server setup, and create async handler utility

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import mongoose from "mongoose";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running on port: ${process.env.PORT}`)});
+    })
+.catch((err) => {
+    console.log("MONGODB Connection failed !!! ", err);
+})
