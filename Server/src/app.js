1ae57f92b9401ecb02a45d4dde09b1223255ff01
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-01 18:24:40 +0530

    fix: correct formatting and improve readability in app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,14 +1,16 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CROSS_ORIGIN,
-    credential: true
-}))
+    credential: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-import todoRoute from "./routes/todo.routes.js"
-app.use('/todos/api/v1/', todoRoute);
-export {app};
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+import todoRoute from "./routes/todo.routes.js";
+app.use("/todos/api/v1/", todoRoute);
+export { app };
