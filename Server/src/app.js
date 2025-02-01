dea52343fb874eb33ba1fca8993271a2d20f9a54
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-01 18:23:58 +0530

    feat: add Todo routes to the server and fix credential typo in CORS configuration

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,13 +1,14 @@
 import express from "express"
 import cors from "cors"
-import cookieParser from "cookie-parser"
 const app = express();
 
 app.use(cors({
     origin: process.env.CROSS_ORIGIN,
-    Credential: true
+    credential: true
 }))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
+import todoRoute from "./routes/todo.routes.js"
+app.use('/todos/api/v1/', todoRoute);
 export {app};
