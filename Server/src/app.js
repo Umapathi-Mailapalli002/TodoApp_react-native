6ad17469251a364d31af4c7a8204d9b21854e3cb
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-02 22:16:53 +0530

    fix: update Todo API route path for consistency

--- Server/src/app.js
+++ Server/src/app.js
@@ -12,5 +12,5 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 import todoRoute from "./routes/todo.routes.js";
-app.use("/todos/api/v1/", todoRoute);
+app.use("/api/v1/todos/", todoRoute);
 export { app };
