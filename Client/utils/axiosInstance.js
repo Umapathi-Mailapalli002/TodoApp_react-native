fb16460aad81167bee1f36302686ed9ecf10c601
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-03 15:59:36 +0530

    fix: update environment variable paths in babel config and axios instance

--- Client/utils/axiosInstance.js
+++ Client/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+import {API_BASE_URL} from "@env"
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_BASE_URL,
+    baseURL: API_BASE_URL,
     withCredentials: true,
 })
 
