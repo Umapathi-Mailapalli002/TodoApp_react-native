881d73694a7ca3c6d2aa4ea73760fb53b555efe2
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-03 10:35:10 +0530

    feat: add toggleCompleted route to Todo API for updating completion status

--- Server/src/routes/todo.routes.js
+++ Server/src/routes/todo.routes.js
@@ -3,12 +3,17 @@ import {
   createTodo,
   deleteTodo,
   getAllTodos,
+  toggleCompleted,
   updateTodo,
 } from "../controllers/todo.controllers";
 
 const router = Router();
 
 router.route("/").get(getAllTodos).post(createTodo);
-router.route("/:id").delete(deleteTodo).patch(updateTodo);
+router
+  .route("/:id")
+  .delete(deleteTodo)
+  .patch(updateTodo)
+  .patch(toggleCompleted);
 
 export default router;
