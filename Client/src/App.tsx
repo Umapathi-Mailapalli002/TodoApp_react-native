f97438820c4e3f72c6abeeaefb88d385109640ee
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-03 16:20:49 +0530

    feat: restructure App component to include heading and update Todo component to JSX

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,26 @@
 import { store } from "../store/index";
 import { Provider } from "react-redux";
 import Todo from "./Todo";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 
 const App = () => {
   return (<Provider store={store}>
-    <Todo />
+    <SafeAreaView>
+      <View>
+        <Text style={styles.heading}>Todo App</Text>
+      <Todo />
+      </View>
+    </SafeAreaView>
   </Provider>)
 }
 
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 30,
+    color: "#fff",
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 10
+  }
+})
 export default App;
