88a848bf57bd52e3b2822ae47acafce2bae3f4fd
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-02-03 10:35:17 +0530

    feat: implement CRUD operations for todos in todoSlice

--- Client/src/features/todoSlice.js
+++ Client/src/features/todoSlice.js
@@ -1,8 +1,69 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axiosInstance from "../../utils/axiosInstance";
 
-const createTodo = createAsyncThunk('todos/createTodo', async()=> {
+export const createTodo = createAsyncThunk('todos/createTodo', async({title, content}, {rejectWithValue})=> {
+   try {
+     const response = await axiosInstance.post('/', {title, content})
+     return response.data;
+   } catch (error) {
+    rejectWithValue(
+        error.response?.data?.message ||
+          error.message ||
+          "Error on adding todo"
+    )
+   }
+})
 
+export const getAllTodos = createAsyncThunk('todos/getAllTodos', async(_,rejectWithValue) => {
+    try {
+        const response = await axiosInstance.get('/')
+        return response.data;
+    } catch (error) {
+        rejectWithValue(
+            error.response?.data?.message ||
+              error.message ||
+              "Error on fetching todos"
+        )
+    }
+})
+
+export const updateTodo = createAsyncThunk('todos/updateTodo', async({title, content, id}, rejectWithValue) => {
+    try {
+        const response = await axiosInstance.patch(`/${id}`, {title, content})
+        return response.data;
+    } catch (error) {
+        rejectWithValue(
+            error.response?.data?.message ||
+              error.message ||
+              "Error on updating todo"
+        )
+    }
+})
+
+export const deleteTodo = createAsyncThunk('todos/deleteTodo', async({id}, {rejectWithValue}) => {
+    try {
+        const response = await axiosInstance.delete(`/${id}`)
+        return response.data;
+    } catch (error) {
+        rejectWithValue(
+            error.response?.data?.message ||
+              error.message ||
+              "Error on deleting todo"
+        ) 
+    }
+})
+
+export const toggleComplete = createAsyncThunk('todos/toggleComplete', async({id, isCompleted}, rejectWithValue) => {
+    try {
+        const response = await axiosInstance.post(`/${id}`, {isCompleted})
+        return response.data;
+    } catch (error) {
+        rejectWithValue(
+            error.response?.data?.message ||
+              error.message ||
+              "Error on toggling as completed todo"
+        ) 
+    }
 })
 const initialState = {
     todos: [],
